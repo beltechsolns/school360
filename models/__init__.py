dc3f69999d3666bf9ca6c5c34061a190dc793624
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2026-02-16 21:43:36 +0300

    fix the technical name and the icon

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import school360_thread_mixin
-from . import school360_academic_year
-from . import school360_res_config_settings
--- models/school360_thread_mixin.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class School360ThreadMixin(models.AbstractModel):
-    _name = 'school360.thread.mixin'
-    _description = 'School360 Logging & Activity Mixin'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
