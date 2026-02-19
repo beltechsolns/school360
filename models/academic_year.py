78a2bc6a5ae53b878f7a86c1ff08e08759387d01
Umer <abuhafs1433@gmail.com>
2026-02-19 11:59:24 +0300

    Remove unnecessary files and folders

--- models/academic_year.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class AcademicYear(models.Model):
-    _inherit = 'school360.academic.year'
-
-    term_ids = fields.One2many('school360.academic.term', 'academic_year_id', string='Terms')
