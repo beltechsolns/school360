283b295fd07020b5ea07ed6095870158c5883a47
Yonas Negese M <yonasnegese250@gmail.com>
2026-02-20 22:42:41 +0300

    remove school academic

--- school360_academic/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import academic_year
-from . import academic_term
-from . import academic_grade
-from . import academic_section
-from . import academic_subject
-from . import academic_timetable
--- school360_academic/models/academic_year.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class AcademicYear(models.Model):
-    _inherit = 'school360_base.academic.year'
-
-    term_ids = fields.One2many('school360_academic.term', 'academic_year_id', string='Terms')
